a1cee9c6ac28a6716c0253d482375bd542de7a3e
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-11-25 22:12:17 -0500

    ...

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
 (function () {
 
-    var ON = true;
-    var OFF = false;
+    var PROCESSING = 0;
+    var ON = 1;
+    var OFF = 2;
     var tabsState = {};
 
     function resetTab(tabId) {
+        console.log('RESET', tabId);
         delete tabsState[tabId];
+        console.log(tabsState);
+
+
     }
 
     chrome.tabs.onUpdated.addListener(resetTab);
@@ -14,27 +19,30 @@
 
     chrome.browserAction.onClicked.addListener(function (tab) {
         var tabId = tab.id;
-
         if (typeof tabsState[tabId] === 'undefined') {
+            tabsState[tabId] = PROCESSING;
             chrome.tabs.executeScript({
                 file: 'main.js'
+            }, function () {
+                chrome.tabs.executeScript({
+                    code: 'main.showOverlays()'
+                }, function () {
+                    tabsState[tabId] = ON;
+                });
             });
-            chrome.tabs.executeScript({
-                code: 'main.showOverlays()'
-            });
-            tabsState[tabId] = ON;
         } else if (tabsState[tabId] === ON) {
             chrome.tabs.executeScript({
                 code: 'main.hideOverlays()'
+            }, function () {
+                tabsState[tabId] = OFF;
             });
-            tabsState[tabId] = OFF;
-        } else {
+        } else if (tabsState[tabId] === OFF) {
             chrome.tabs.executeScript({
                 code: 'main.showOverlays()'
+            }, function () {
+                tabsState[tabId] = ON;
             });
-            tabsState[tabId] = ON;
         }
-
     });
 
 })();
